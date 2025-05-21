1689b32f3c236c31130d705743ffd38ce14198f8
F-Naqvi <fnaqvi8@icloud.com>
2025-05-21 20:06:58 +0000

    Create adventure file

---
+++ choose_your_own_adventure.py
@@ -0,0 +1,12 @@
+name = input("please type your name: ")
+print("Welcome", name, "to this adventure!")
+
+answer = input("You are on a dirt road. It has come to an end and you can go left or right. Which way would you like to go? ").lower()
+
+if answer == "left":
+
+elif answer == "right":
+    print("")
+
+else:
+    print("Invalid answer. You lose.")
